ba8f5b19e5506e2cedad40715376d002bda2021a
mathcovax <campani.mathieu@gmail.com>
2023-10-25 14:54:41 +0000

    [fix] tailwind

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./templates/**/*.html.twig"],
+	content: [
+		"./assets/**/*.js",
+		"./templates/**/*.html.twig",
+	],
 	theme: {
 	  extend: {},
 	},
