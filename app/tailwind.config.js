17c9b94c29251d36eaa3bbf315a4dc19a800a219
Maubry94 <matthieu.aubry94370@gmail.com>
2023-11-15 13:38:23 +0100

    [UPDATE]: homepage

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
 		"./templates/**/*.html.twig",
 	],
 	theme: {
-	  extend: {},
+		extend: {
+			borderRadius: {
+				"large": "140px"
+			},
+			colors: {
+				"darker-green": "#374B43",
+				"green": "#87B5A2",
+				"lighter-green": "#B1C3BB",
+				"darker-white": "#EFEFEF",
+			},
+		}
 	},
-	plugins: [],
+	plugins: []
 };
