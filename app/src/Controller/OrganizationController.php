1a93696415dafc165f7bfdedccd1e753e641d98a
Vitalx <liamdu92@gmail.com>
2023-11-23 19:21:17 +0100

    [ADD] redirect route when no token for create organization

--- app/src/Controller/OrganizationController.php
+++ app/src/Controller/OrganizationController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Organization;
+use App\Entity\User;
 use App\Form\CreateOrganizationFormType;
 use App\Services\Organization\OrganizationService;
+use App\Services\Token\AccessTokenService;
+use App\Services\User\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
@@ -26,27 +29,52 @@ class OrganizationController extends AbstractController
 			methods: ["GET", "POST"]
 		)
 	]
-	public function create(Request $request): Response
+	public function create(Request $request, UserService $userService): Response
 	{
+		$response = new Response();
 		$organization = new Organization();
 		$form = $this->createForm(
 			CreateOrganizationFormType::class,
 			$organization
 		);
-		$response = new Response();
-		$form->handleRequest($request);
-		if ($form->isSubmitted() && $form->isValid()) {
-			/** @var Organization */
-			$organization = $form->getData();
-			try {
-				$this->organizationService->createOrganization($organization);
-				$response->setStatusCode(Response::HTTP_OK);
-				$this->addFlash("success", "L'organisation à bien été créée.");
-			} catch (\Exception $e) {
-				$response->setStatusCode(Response::HTTP_BAD_REQUEST);
-				$this->addFlash("error", $e->getMessage());
+
+		if (AccessTokenService::extractCookie($request->cookies)) {
+			$form->handleRequest($request);
+
+			if ($form->isSubmitted() && $form->isValid()) {
+				/** @var Organization */
+				$organization = $form->getData();
+				$payloadCookie = AccessTokenService::extractCookie(
+					$request->cookies
+				);
+				die($payloadCookie);
+				$userId = $payloadCookie["data"];
+				/** @var User */
+				$user = $userService->getById($userId);
+				$organization->setCreatedBy($user);
+
+				try {
+					$this->organizationService->createOrganization(
+						$organization
+					);
+					$response->setStatusCode(Response::HTTP_OK);
+					$this->addFlash(
+						"success",
+						"L'organisation à bien été créée."
+					);
+				} catch (\Exception $e) {
+					$response->setStatusCode(Response::HTTP_BAD_REQUEST);
+					$this->addFlash("error", $e->getMessage());
+				}
 			}
+		} else {
+			return $this->redirectToRoute(
+				"user_login",
+				[],
+				Response::HTTP_FOUND
+			);
 		}
+
 		return $this->render(
 			"organization/index.html.twig",
 			[
