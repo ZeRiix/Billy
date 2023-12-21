9402240ea30b1b2120d7c4d0ce38bef18fb0c16b
mathcovax <campani.mathieu@gmail.com>
2023-12-21 11:43:00 +0000

    fix address

--- newapp/src/Controller/RegistrationController.php
+++ newapp/src/Controller/RegistrationController.php
@@ -47,7 +47,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('billy.esgi@gmail.com', 'billy'))
+                    ->from(new Address('support@billy.com', 'billy'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
