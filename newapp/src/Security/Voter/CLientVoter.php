d2254b57e574f01acb64616a7f70bdd648e894e3
mathcovax <campani.mathieu@gmail.com>
2023-12-22 16:10:24 +0000

    add client voter

---
+++ newapp/src/Security/Voter/CLientVoter.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Security\Voter;
+
+use App\Entity\Client;
+use App\Entity\Organization;
+use App\Repository\RoleRepository;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class ClientVoter extends Voter
+{
+    public const CREATE = 'CLIENT_CREATE';
+    public const UPDATE = 'CLIENT_UPDATE';
+
+	public function __construct(
+		private RoleRepository $roleRepository
+	)
+	{}
+
+    protected function supports(string $attribute, mixed $subject): bool
+    {
+        return (
+			in_array($attribute, [self::CREATE, self::UPDATE]) && (
+				$subject instanceof Organization ||
+				$subject instanceof Client
+			)
+		);
+    }
+
+    protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool
+    {
+        $user = $token->getUser();
+        if (!$user instanceof UserInterface) {
+            return false;
+        }
+
+		if($attribute === self::CREATE) {
+			/** @var Organization $subject */
+			return $this->roleRepository->checkPermissionOnOrganization($user, $subject, "manage_client");
+		}
+		else if($attribute === self::UPDATE){
+			/** @var Client $subject */
+			$organization = $subject->getOrganization();
+			if(!$organization) return false;
+			return $this->roleRepository->checkPermissionOnOrganization($user, $organization, "manage_client");
+		}
+		else return false;
+    }
+}
