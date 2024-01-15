56a10edd8446ffe355b53d4be65cd0560b8341e3
mathcovax <campani.mathieu@gmail.com>
2024-01-15 09:50:42 +0000

    add flash error when wrong id login

--- newapp/src/Controller/SecurityController.php
+++ newapp/src/Controller/SecurityController.php
@@ -23,6 +23,10 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+		if($error){
+			$this->addFlash("error", "Mots de pass ou adresse email incorrect.");
+		}
+
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
