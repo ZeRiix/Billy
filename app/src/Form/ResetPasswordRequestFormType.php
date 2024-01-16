ed5abff66945590e4eeb7754d1675cfcd46cefd2
Maubry94 <matthieu.aubry94370@gmail.com>
2024-01-16 11:35:22 +0100

    [UPDATE]: base template

--- app/src/Form/ResetPasswordRequestFormType.php
+++ app/src/Form/ResetPasswordRequestFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -21,6 +22,9 @@ class ResetPasswordRequestFormType extends AbstractType
                     ]),
                 ],
             ])
+			->add("submit", SubmitType::class, [
+				"label" => "Envoyer le lien de réinitialisation",
+			]);
         ;
     }
 
