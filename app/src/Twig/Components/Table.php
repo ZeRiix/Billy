d217311a9fc307a5c9e0fe3d88464e0f2d7f9a19
Maubry94 <matthieu.aubry94370@gmail.com>
2024-01-19 13:45:26 +0100

    [ADD]: table component

---
+++ app/src/Twig/Components/Table.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Twig\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent()]
+class Table{
+	public bool $action;
+	public mixed $data;
+	public array $cols;
+}
