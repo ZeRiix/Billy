2e65265bbb6d31d7ed87f74601bc8449fffafafc
mathcovax <campani.mathieu@gmail.com>
2024-02-09 15:15:32 +0000

    [feat] image side bar

--- app/src/Twig/AppExtension.php
+++ app/src/Twig/AppExtension.php
@@ -18,48 +18,56 @@ use Twig\TwigFunction;
 class AppExtension extends AbstractExtension
 {
 	private Serializer $serializer;
+	private ?Organization $organization;
 
 	public function __construct(
 		private RoleRepository $roleRepository,
 		private OrganizationRepository $organizationRepository,
 		private RequestStack $requestStack
-	)
-	{
+	) {
 		$classMetadataFactory = new ClassMetadataFactory(new AttributeLoader());
-		$this->serializer = new Serializer([new UidNormalizer(), new ObjectNormalizer($classMetadataFactory)]);
+		$this->serializer = new Serializer([
+			new UidNormalizer(),
+			new ObjectNormalizer($classMetadataFactory),
+		]);
 	}
 
-    public function getFunctions(): array
-    {
-        return [
-            new TwigFunction('checkPermissionOnOrganization', [$this, 'checkPermissionOnOrganization']),
-			new TwigFunction('getCurrentOrganization', [$this, 'getCurrentOrganization']),
-			new TwigFunction('entityToJson', [$this, 'entityToJson']),
-        ];
-    }
+	public function getFunctions(): array
+	{
+		return [
+			new TwigFunction("checkPermissionOnOrganization", [$this, "checkPermissionOnOrganization"]),
+			new TwigFunction("getCurrentOrganization", [$this, "getCurrentOrganization"]),
+			new TwigFunction("entityToJson", [$this, "entityToJson"]),
+		];
+	}
 
-    public function checkPermissionOnOrganization(User $user, string $permission): bool
-    {
+	public function checkPermissionOnOrganization(User $user, string $permission): bool
+	{
 		$organization = $this->getCurrentOrganization();
-		if(!$organization){
+		if (!$organization) {
 			return false;
 		}
 
-        return $this->roleRepository->userHasPermission($organization, $user,  $permission);
-    }
+		return $this->roleRepository->userHasPermission($organization, $user, $permission);
+	}
 
 	public function getCurrentOrganization(): ?Organization
 	{
-		$request = $this->requestStack->getMainRequest();
-		$organizationId = $request->get("organization");
-		if(!$organizationId){
-			return null;
+		if (!empty($this->organization)) {
+			return $this->organization;
 		}
 
-		return $this->organizationRepository->find($organizationId);
+		$request = $this->requestStack->getMainRequest();
+		$organizationId = $request->get("organization");
+		if (!$organizationId) {
+			return $this->organization = null;
+		}
+
+		return $this->organization = $this->organizationRepository->find($organizationId);
 	}
 
-	public function entityToJson(mixed $entity, array $groups) {
-		return json_encode($this->serializer->normalize($entity, null, ['groups' => $groups]));
+	public function entityToJson(mixed $entity, array $groups)
+	{
+		return json_encode($this->serializer->normalize($entity, null, ["groups" => $groups]));
 	}
 }
