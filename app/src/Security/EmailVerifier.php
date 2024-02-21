c40279e80a73cef5c832bde8f8e6ab5ee795bfa4
zeriix <wflorentin@myges.fr>
2024-02-21 18:01:22 +0100

    - [ADD]: adding google mailer on route register/reset/invite

--- app/src/Security/EmailVerifier.php
+++ app/src/Security/EmailVerifier.php
@@ -10,44 +10,66 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
 
+use Symfony\Component\Mailer\Bridge\Google\Transport\GmailSmtpTransport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mime\Email;
+use Twig\Environment;
+use Symfony\Component\Mime\Address;
+
 class EmailVerifier
 {
-    public function __construct(
-        private VerifyEmailHelperInterface $verifyEmailHelper,
-        private MailerInterface $mailer,
-        private EntityManagerInterface $entityManager
-    ) {
-    }
+	public function __construct(
+		private VerifyEmailHelperInterface $verifyEmailHelper,
+		private MailerInterface $mailer,
+		private EntityManagerInterface $entityManager,
+		private Environment $twig
+	) {
+	}
 
-    public function sendEmailConfirmation(string $verifyEmailRouteName, UserInterface $user, TemplatedEmail $email): void
-    {
-        $signatureComponents = $this->verifyEmailHelper->generateSignature(
-            $verifyEmailRouteName,
-            $user->getId(),
-            $user->getEmail(),
-			['id' => $user->getId()]
-        );
+	public function sendEmailConfirmation(
+		string $verifyEmailRouteName,
+		UserInterface $user,
+		TemplatedEmail $email
+	): void {
+		$signatureComponents = $this->verifyEmailHelper->generateSignature(
+			$verifyEmailRouteName,
+			$user->getId(),
+			$user->getEmail(),
+			["id" => $user->getId()]
+		);
 
-        $context = $email->getContext();
-        $context['signedUrl'] = $signatureComponents->getSignedUrl();
-        $context['expiresAtMessageKey'] = $signatureComponents->getExpirationMessageKey();
-        $context['expiresAtMessageData'] = $signatureComponents->getExpirationMessageData();
+		$context = $email->getContext();
+		$context["signedUrl"] = $signatureComponents->getSignedUrl();
+		$context["expiresAtMessageKey"] = $signatureComponents->getExpirationMessageKey();
+		$context["expiresAtMessageData"] = $signatureComponents->getExpirationMessageData();
 
-        $email->context($context);
+		$email->context($context);
+		$htmlContent = $this->twig->render($email->getHtmlTemplate(), $email->getContext());
 
-        $this->mailer->send($email);
-    }
+		$send = (new Email())
+			->from(new Address("billy.esgi@gmail.com", "support-billy@gmail.com"))
+			->to($user->getEmail())
+			->subject("Please Confirm your Email")
+			->html($htmlContent);
+		(new Mailer(new GmailSmtpTransport("billy.esgi@gmail.com", "qmrp leef onim orrj")))->send($send);
 
-    /**
-     * @throws VerifyEmailExceptionInterface
-     */
-    public function handleEmailConfirmation(Request $request, UserInterface $user): void
-    {
-        $this->verifyEmailHelper->validateEmailConfirmation($request->getUri(), $user->getId(), $user->getEmail());
+		//$this->mailer->send($email);
+	}
 
-        $user->setIsVerified(true);
+	/**
+	 * @throws VerifyEmailExceptionInterface
+	 */
+	public function handleEmailConfirmation(Request $request, UserInterface $user): void
+	{
+		$this->verifyEmailHelper->validateEmailConfirmation(
+			$request->getUri(),
+			$user->getId(),
+			$user->getEmail()
+		);
 
-        $this->entityManager->persist($user);
-        $this->entityManager->flush();
-    }
+		$user->setIsVerified(true);
+
+		$this->entityManager->persist($user);
+		$this->entityManager->flush();
+	}
 }
--- app/src/Services/MailService.php
+++ app/src/Services/MailService.php
@@ -2,22 +2,28 @@
 
 namespace App\Services;
 
+use Symfony\Component\Mailer\Bridge\Google\Transport\GmailSmtpTransport;
 use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mime\Email;
-use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mime\Address;
 
 class MailService
 {
 	public static function send(string $to, string $subject, string $body, bool $isContact): void
 	{
-		$email = self::create($to, $subject, $body, $isContact ? "MAILER_CONTACT_FROM" : "MAILER_NOREPLY_FROM");
-		(new Mailer(Transport::fromDsn($_ENV["MAILER_DSN"])))->send($email);
+		$email = self::create(
+			$to,
+			$subject,
+			$body,
+			$isContact ? "MAILER_CONTACT_FROM" : "MAILER_NOREPLY_FROM"
+		);
+		(new Mailer(new GmailSmtpTransport("billy.esgi@gmail.com", "qmrp leef onim orrj")))->send($email);
 	}
 
 	private static function create(string $to, string $subject, string $body, string $from)
 	{
 		return (new Email())
-			->from($_ENV[$from])
+			->from(new Address("billy.esgi@gmail.com", $_ENV[$from]))
 			->to($to)
 			->subject($subject)
 			->html($body);
